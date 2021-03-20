d95f3264f623a4dc583035365a704aa849c8eeb9
Aashka Doshi <aashkadoshi7@gmail.com>
2021-03-21 00:53:44 +0530

    Added Dashboard Table UI

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,27 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SignIn from './Components/Auth/SignIn'
 import SignUp from './Components/Auth/SignUp'
 import DashboardTable from './Components/Dashboard/DashboardTable'
+import Header from './Components/Dashboard/Header'
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path='/signup'>
-          <SignUp></SignUp>
-        </Route>
-        <Route path='/signin'>
-          <SignIn></SignIn>
-        </Route>
-        <Route path='/DashboardTable'>
-          <DashboardTable></DashboardTable>
-        </Route>
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path='/signup'>
+                    <SignUp></SignUp>
+                </Route>
+                <Route path='/signin'>
+                    <SignIn></SignIn>
+                </Route>
+                <Route path='/Header'>
+                    <Header></Header>
+                </Route>
+                <Route path='/DashboardTable'>
+                    <DashboardTable></DashboardTable>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
