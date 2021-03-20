879bfeaaa58eb7e0cfdf94e93c2f55a0ae7d86a5
Aashka Doshi <aashkadoshi7@gmail.com>
2021-03-21 02:05:30 +0530

    header

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import SignIn from './Components/Auth/SignIn'
 import SignUp from './Components/Auth/SignUp'
 import Products from './Components/Products/Products'
 import DashboardTable from './Components/Dashboard/DashboardTable'
-<<<<<<< HEAD
+import Categories from './Components/Categories/Categories'
 import Header from './Components/Dashboard/Header'
 
 function App() {
@@ -18,7 +18,13 @@ function App() {
                 <Route path='/signin'>
                     <SignIn></SignIn>
                 </Route>
-                <Route path='/Header'>
+                <Route path='/category'>
+                    <Categories></Categories>
+                </Route>
+                <Route path='/product'>
+                    <Products></Products>
+                </Route>
+                <Route path="/Header">
                     <Header></Header>
                 </Route>
                 <Route path='/DashboardTable'>
@@ -27,32 +33,6 @@ function App() {
             </Switch>
         </Router>
     );
-=======
-import Categories from './Components/Categories/Categories'
-function App() {
-  return (
-    <Router>
-      <Switch>
-
-      <Route path='/signup'>
-           <SignUp></SignUp>
-        </Route>
-        <Route path='/signin'>
-          <SignIn></SignIn>
-        </Route>
-        <Route path='/category'>
-          <Categories></Categories>
-        </Route>
-        <Route path='/product'>
-          <Products></Products>
-        </Route>
-        <Route path='/DashboardTable'>
-          <DashboardTable></DashboardTable>
-          </Route>
-      </Switch>
-    </Router>
-  );
->>>>>>> af108d0ff88605fb752581539430b48feb799a32
 }
 
 export default App;
