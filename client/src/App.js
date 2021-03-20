2acf1a8cad4647c15e5e35120cfe88fdc34bd9dc
Nidhish Mehra <nidhishmehra29@gmail.com>
2021-03-21 01:35:12 +0530

    Bug

--- client/src/App.js
+++ client/src/App.js
@@ -5,35 +5,13 @@ import SignIn from './Components/Auth/SignIn'
 import SignUp from './Components/Auth/SignUp'
 import Products from './Components/Products/Products'
 import DashboardTable from './Components/Dashboard/DashboardTable'
-<<<<<<< HEAD
-import Header from './Components/Dashboard/Header'
-
-function App() {
-    return (
-        <Router>
-            <Switch>
-                <Route path='/signup'>
-                    <SignUp></SignUp>
-                </Route>
-                <Route path='/signin'>
-                    <SignIn></SignIn>
-                </Route>
-                <Route path='/Header'>
-                    <Header></Header>
-                </Route>
-                <Route path='/DashboardTable'>
-                    <DashboardTable></DashboardTable>
-                </Route>
-            </Switch>
-        </Router>
-    );
-=======
 import Categories from './Components/Categories/Categories'
+import SideNav from './Components/SideNav';
 function App() {
   return (
     <Router>
       <Switch>
-
+      <SideNav/>
       <Route path='/signup'>
            <SignUp></SignUp>
         </Route>
@@ -52,7 +30,6 @@ function App() {
       </Switch>
     </Router>
   );
->>>>>>> af108d0ff88605fb752581539430b48feb799a32
 }
 
 export default App;
